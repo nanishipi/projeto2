da6aa04d1be0c133e725b96de0d40cc53ca2ca75
fabianotorrao <56256925+fabianotorrao@users.noreply.github.com>
2021-01-12 20:52:11 +0000

    Add files via upload

--- src/views/CriarProposta.vue
+++ src/views/CriarProposta.vue
@@ -1,15 +1,56 @@
 <template>
-  <div id="app">
-      <h1>This is an Criar Propostas page</h1>
+  <div>
+    <p id="title" v-if="estagio" class="ml-5">Criar Proposta Estágio</p>
+    <p id="title" v-else class="ml-5">Criar Proposta Projeto</p>
+
+    <step_one v-if="steps == 1" />
+    <step_two v-if="steps == 2"></step_two>
+    <step_three v-if="steps == 3">step3</step_three>
   </div>
 </template>
 
 <script>
+import step_one from "../components/proposta_form/step_one.vue";
+import step_two from "../components/proposta_form/step_two.vue";
+import step_three from "../components/proposta_form/step_three.vue";
 export default {
-
-}
+  components: {
+    step_one,
+    step_two,
+    step_three,
+  },
+  data() {
+    return {
+      estagio: true,
+      steps: 1,
+      form_proposta: {
+        titulo: "",
+        objetivos: "",
+        resultados: "",
+        dados_relevantes: "",
+        recursos: "",
+        plano: "",
+        entidade: null,
+        tutor: "",
+        email: "",
+        cargo: "",
+        contato: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+  },
+};
 </script>
 
 <style>
-
+.input {
+  border-radius: 15px;
+  box-shadow: 2px 2px 2px 2px #e6e6e6;
+  border: none;
+}
 </style>
